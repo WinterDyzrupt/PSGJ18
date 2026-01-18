28b9ba621821fd8fa80f7b9ae37b6560407dedf6
Winter Dyzrupt <Winter@DyzruptGames.com>
2026-01-17 18:23:08 -0800

    Title Scene Settings Prefab Added TMP Text and UI Toolkit which has a lot of files it wanted to stuff in here that can't seem to be removed. Fairly small, so just going to leave it.

---
+++ Assets/Generic/Scripts/ManagerSettings.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using Variables;
+
+namespace Generic.Scripts
+{
+    public class SettingsManager : MonoBehaviour
+    {
+        [SerializeField] private BoolVariable settingBool; 
+        
+        [SerializeField] private GameObject settingsPrefab;
+
+        private void Start()
+        {
+            Debug.Assert(settingBool != null, "SettingBool is null! Assign in inspector!");
+            
+            Debug.Assert(settingsPrefab != null, "SettingsPrefab is null! Assign in inspector!.");
+
+            settingBool.BoolChanged += ToggleSettingsPanel;
+        }
+        
+        private void ToggleSettingsPanel()
+        {
+            settingsPrefab.SetActive(!settingsPrefab.activeSelf);
+        }
+        
+        // TODO: Create Control options here. Probably Sound, Music, Controls, etc
+    }
+}
