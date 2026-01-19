7f4ba133c56119d788b3da5315889e951ed5558c
WinterDyzrupt <winter@dyzruptgames.com>
2026-01-18 18:19:45 -0800

    Title Scene and Settings Prefab (#2)
    * Title Scene
    Settings Prefab
    Added TMP Text and UI Toolkit which has a lot of files it wanted to stuff in here that can't seem to be removed. Fairly small, so just going to leave it.
    
    * Added Rudimentary Playmat scene and simple layout. (#3)

---
+++ Assets/Generic/Scripts/SceneData.cs
@@ -0,0 +1,8 @@
+namespace Generic.Scripts
+{
+    public static class SceneData
+    {
+        public const int TitleSceneIndex = 0;
+        public const int PlaymatSceneIndex = 1;
+    }
+}
---
+++ Assets/SceneTitle/Scripts/ManagerTitle.cs
@@ -0,0 +1,52 @@
+using Generic.Scripts;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Variables;
+
+namespace SceneTitle.Scripts
+{
+    public class ManagerTitle : MonoBehaviour
+    {
+        [SerializeField] private BoolVariable startBool;
+        [SerializeField] private BoolVariable manualBool;
+        [SerializeField] private BoolVariable creditsBool;
+
+        [SerializeField] private GameObject manualPanel;
+        [SerializeField] private GameObject creditsPanel;
+
+        private void Start()
+        {
+            Debug.Assert(startBool != null, "StartBool is null! Assign in inspector!");
+            Debug.Assert(manualBool != null, "ManualBool is null! Assign in inspector!");
+            Debug.Assert(creditsBool != null, "CreditsBool is null! Assign in inspector!");
+        
+            Debug.Assert(manualPanel != null, "ManualPanel is null! Assign in inspector!");
+            Debug.Assert(creditsPanel != null, "CreditsPanel is null! Assign in inspector!");
+
+            startBool.BoolChanged += StartButton;
+            manualBool.BoolChanged += ManualButton;
+            creditsBool.BoolChanged += CreditsButton;
+        }
+
+        private static void StartButton()
+        {
+            SceneManager.LoadScene(SceneData.PlaymatSceneIndex);
+        }
+
+        private void ManualButton()
+        {
+            if (manualPanel.activeSelf != manualBool)
+            {
+                manualPanel.SetActive(manualBool);
+            }
+        }
+
+        private void CreditsButton()
+        {
+            if (creditsPanel.activeSelf != creditsBool)
+            {
+                creditsPanel.SetActive(creditsBool);
+            }
+        }
+    }
+}
