879d05f5d250bd5504c08391c336a141aa765b36
Kajure <mjmthere@gmail.com>
2026-01-22 21:04:21 -0800

    Import GameEvent stuff from last project... we may want to make a common package. (#10)

---
+++ Assets/Common/Utils/Events/EventEditor.cs
@@ -0,0 +1,26 @@
+namespace Common.Utils.Events
+{
+    using UnityEditor;
+    using UnityEngine;
+
+    [CustomEditor(typeof(GameEvent), editorForChildClasses: true)]
+    public class EventEditor : Editor
+    {
+        /// <summary>
+        /// When inspecting an GameEvent, shows a button to raise the event for easy testing of events without triggering
+        /// their conditions.
+        /// </summary>
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            GUI.enabled = Application.isPlaying;
+
+            var eventToInspect = target as GameEvent;
+            if (GUILayout.Button("Raise"))
+            {
+                eventToInspect.Raise();
+            }
+        }
+    }
+}
---
+++ Assets/Common/Utils/Events/GameEventListener.cs
@@ -0,0 +1,34 @@
+namespace Common.Utils.Events
+{
+    using UnityEngine;
+    using UnityEngine.Events;
+
+    public class GameEventListener : MonoBehaviour
+    {
+        /// <summary>
+        /// The specific type of event to respond to.
+        /// </summary>
+        public GameEvent Event;
+
+        /// <summary>
+        /// The function call to respond with.
+        /// </summary>
+        public UnityEvent Response;
+
+        private void OnEnable()
+        {
+            Event.RegisterListener(this);
+        }
+
+        private void OnDisable()
+        {
+            Event.UnregisterListener(this);
+        }
+
+        public void OnEventRaised()
+        {
+            Response.Invoke();
+        }
+
+    }
+}
